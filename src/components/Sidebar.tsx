a4b3756c91ecd46a656580c3d0f681f4f1526f22
Mario Ciau <LE16081484@merida.tecnm.mx>
2024-06-06 17:35:21 -0600

    cambios sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -531,13 +531,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Profile --> */}
             </ul>
           </div>
-
           {/* <!-- Others Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               OTROS
             </h3>
-
             <ul className="mb-6 flex flex-col gap-1.5"></ul>
           </div>
         </nav>
